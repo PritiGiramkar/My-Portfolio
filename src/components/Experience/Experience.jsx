05b951b4351cc9abbb65a891d811644b8772d94b
Priti Giramkar <pritigiramkar20@gmail.com>
2024-09-08 16:39:51 +0530

    added logos

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -8,19 +8,7 @@ export const Experience = () => {
     <section id='experience' className={styles.container}>
       <h2 className={styles.title}>Experience</h2>
         <div className={styles.content}>
-          <div className={styles.skills}>
-            {
-              skills.map((skills,id)=>{
-                return  <div key={id} className={styles.skill}>
-                  <div className={styles.skillImageContainer}>
-                    <img src={getImageUrl(skills.imagesrc)} alt={skills.title} />
-                    </div>
-                    <p>{skills.title}</p>
-                </div>
-              } )
-            }
-          </div>
-          <ul className={styles.history}>
+        <ul className={styles.history}>
             {
               history.map((historyitem,id)=>{
                 return <li key={id} className={styles.historyitem}>
@@ -38,6 +26,59 @@ export const Experience = () => {
               })
             }
           </ul>
+          <h1 className={styles.subtitle}>Programming Languages</h1>
+          <div className={styles.skills}>
+            {
+              skills[0].programminglanguages.map((skills,id)=>{
+                return  <div key={id} className={styles.skill}>
+                  <div className={styles.skillImageContainer}>
+                    <img src={getImageUrl(skills.imagesrc)} alt={skills.title} />
+                    </div>
+                    <p>{skills.title}</p>
+                </div>
+              } )
+            }
+          </div>
+          <h1 className={styles.subtitle}>Frameworks & Tools</h1>
+          <div className={styles.skills}>
+            {
+              skills[1].FrameworksAndTools.map((skills,id)=>{
+                return  <div key={id} className={styles.skill}>
+                  <div className={styles.skillImageContainer}>
+                    <img src={getImageUrl(skills.imagesrc)} alt={skills.title} />
+                    </div>
+                    <p>{skills.title}</p>
+                </div>
+              } )
+            }
+          </div>
+          <h1 className={styles.subtitle}>Development Tools</h1>
+          <div className={styles.skills}>
+            {
+              skills[2].DevelopmentTools.map((skills,id)=>{
+                return  <div key={id} className={styles.skill}>
+                  <div className={styles.skillImageContainer}>
+                    <img src={getImageUrl(skills.imagesrc)} alt={skills.title} />
+                    </div>
+                    <p>{skills.title}</p>
+                </div>
+              } )
+            }
+          </div>
+          <h1 className={styles.subtitle}>Cloud Platforms</h1>
+          <div className={styles.skills}>
+            {
+              skills[3].CloudPlatforms.map((skills,id)=>{
+                return  <div key={id} className={styles.skill}>
+                  <div className={styles.skillImageContainer}>
+                    <img src={getImageUrl(skills.imagesrc)} alt={skills.title} />
+                    </div>
+                    <p>{skills.title}</p>
+                </div>
+              } )
+            }
+          </div>
+          
         </div>
     </section>
     
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { getImageUrl } from '../../utils';
 export const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
   return (<nav className={styles.navbar}>
-    <a className={styles.title} href="/">Priti</a>
+    <a className={styles.title} href="/">Portfolio</a>
     <div className={styles.menu}>
         <img className={styles.menuBtn} 
         src={menuOpen ? getImageUrl("nav/closeIcon.png"):getImageUrl("nav/menuIcon.jpeg")} 
